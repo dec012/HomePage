fd7ffbda68b4e94335a004334a59657afbbb4580
rodrigo <72745179+dec012@users.noreply.github.com>
2023-06-08 23:12:05 -0300

    Add files via upload

---
+++ HTML/assets/JS/build.js
@@ -0,0 +1,46 @@
+//bloquear o inspetor de elemento
+
+var message="";
+function clickIE() {if (document.all) {(message);return false;}}
+function clickNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(message);return false;}}}
+if (document.layers)
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+
+document.oncontextmenu=new Function("return false")
+
+
+//  F12
+//==========
+
+  document.onkeypress = function (event) {
+    if (e.ctrlKey &&
+        (e.keyCode === 123)) {
+            // alert('not allowed');
+            return false;
+          }
+  };
+
+
+//    CTRL + u
+//==============
+
+  document.onkeydown = function(e) {
+    if (e.ctrlKey &&
+      (e.keyCode === 85)) {
+          // alert('not allowed');
+          return false;
+        }
+      };  
+
+// Copiar com CTRL+C/Command+C
+
+document.addEventListener('copy', function(e) {
+    e.preventDefault();
+    var selectedText = window.getSelection().toString();
+    var modifiedText = 'Boa garoto: ' + selectedText; // Modifique o texto conforme necessário
+    e.clipboardData.setData('text/plain', modifiedText);
+  });
+  
